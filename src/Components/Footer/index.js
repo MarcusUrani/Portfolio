36b66d837ea49822e59a9fdaa1130562da2b0481
Marcus Urani <marcus.urani20@gmail.com>
2022-01-31 10:58:18 -0300

    Adicionando o projeto Aluracord

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -4,7 +4,14 @@ import "./style.css";
 const Footer = () => {
   return (
     <footer className="footer">
-      <p className="footer__text">Desenvolvido por Marcus Urani</p>
+      <p>Desenvolvido por Marcus Urani</p>
+      <a
+        className="footer__link"
+        href="https://www.flaticon.com/free-icons/"
+        title="Icons"
+      >
+        All icons created by Freepik - Flaticon
+      </a>
     </footer>
   );
 };
